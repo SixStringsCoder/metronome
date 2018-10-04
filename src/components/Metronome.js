7def561e9417ed6626240f9c075bf9fb2e59f4a8
steve hanlon <rshanlon@gmail.com>
2018-10-04 15:14:04 -0700

    Added Time Signature selector to control State for  BeatsPerMeasure; added another row in CSS grid; add method for handling selector change and added HTML

--- src/components/Metronome.js
+++ src/components/Metronome.js
@@ -39,7 +39,11 @@ class Metronome extends Component {
   startStop = () => {
     if (this.state.playing) {
       clearInterval(this.timer);
-      this.setState({ playing: false })
+      this.setState({
+        playing: false,
+        subDivPlaying: false,
+        count: 0
+      })
     } else {
       this.timer = setInterval(this.playClick, 60000 / this.state.bpm);
       this.setState(
@@ -54,18 +58,14 @@ class Metronome extends Component {
   playClick = () => {
     const {count, beatsPerMeasure} = this.state;
     // Put accented beat on 1 and softer beat on other beats
-    count > 0 ? this.click1.play()
-    :
-    this.click2.play();
+    count > 0 ? this.click1.play() : this.click2.play();
 
     this.setState(state => ({
       count: (state.count + 1) % state.beatsPerMeasure
     }));
   }
 
-  playSubDClick = () => this.click3.play();
-
-  startStopSubDiv = (event) => {
+  startStopSubDiv = event => {
     const beat = {
       eighths: (60000 / this.state.bpm) / 2,
       triplets: (60000 / this.state.bpm) / 3,
@@ -76,9 +76,29 @@ class Metronome extends Component {
     if (this.state.subDivPlaying) {
       clearInterval(this.subDivClick);
       this.setState({ subDivPlaying: false });
-    } else {
+    } else if (this.state.playing) {
       this.subDivClick = setInterval(this.playSubDClick, beat[subDivision]);
       this.setState({ subDivPlaying: true });
+    } else {
+      alert("The metronome must be playing to hear subdivions.")
+    }
+  }
+
+  playSubDClick = () => this.click3.play();
+
+  handleTimeSigSelect = (event) => {
+    if (this.state.playing) {
+      clearInterval(this.timer);
+      this.timer = setInterval(this.playClick, 60000 / this.state.bpm);
+      this.setState({
+        count: 0,
+        beatsPerMeasure: parseInt(event.target.value)
+      });
+    } else {
+      this.setState({
+        count: 0,
+        beatsPerMeasure: parseInt(event.target.value)
+      });
     }
   }
 
@@ -98,11 +118,24 @@ class Metronome extends Component {
                 value={bpm} />
             </div>
             <div id="bpmLabel">{bpm} BPM</div>
+
             <button onMouseDown={this.startStop} id="playBtn">{playing ? 'Stop' : 'Play'}</button>
+
+            <div className="timeSigSelect">
+            <label>Beats Per Measure</label>
+              <select onChange={this.handleTimeSigSelect}>
+                <option value="3">3</option>
+                <option value="4" selected>4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+              </select>
+            </div>
+
             <div id="subDivBtnContainer">
-              <a><img onClick={this.startStopSubDiv} className="subDivBtn" id="eighths" src={eighthNotes} /></a>
-              <a><img onClick={this.startStopSubDiv} className="subDivBtn" id="triplets" src={tripletNotes} /></a>
-              <a><img onClick={this.startStopSubDiv} className="subDivBtn" id="sixteenths" src={sixteenthNotes} /></a>
+              <a><img onMouseDown={this.startStopSubDiv} className="subDivBtn" id="eighths" src={eighthNotes} /></a>
+              <a><img onMouseDown={this.startStopSubDiv} className="subDivBtn" id="triplets" src={tripletNotes} /></a>
+              <a><img onMouseDown={this.startStopSubDiv} className="subDivBtn" id="sixteenths" src={sixteenthNotes} /></a>
             </div>
           </div>
         </section>
